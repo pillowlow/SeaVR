31c3f9b58cf99941910899e343daba16222dd03c
pillowlow <tmps9930613@gmail.com>
2022-05-12 16:20:48 +0800

    first commit

---
+++ Assets/Scripts/EnemyRobots/RobotChangeEmo.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class RobotChangeEmo : MonoBehaviour
+{   
+
+    public Texture[] myMaps;
+    public Color[] mycolors;
+    public float[] intensities;
+    public int index = 0;
+    public Material TargetMat;
+
+    private int ChangeDetecter;
+    // Start is called before the first frame update
+    void Start()
+    {   
+
+        if(index > myMaps.Length-1){
+
+            index = 0;
+        }
+        ChangeDetecter = index;
+        TargetMat.SetTexture("_Emission",myMaps[index]);
+
+        float factor = intensities[index]*intensities[index];
+        Color temp  = new Color(mycolors[index].r*factor,mycolors[index].g*factor,mycolors[index].b*factor);
+
+        TargetMat.SetColor("_EmiColor",temp);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        if (ChangeDetecter != index){
+            TargetMat.SetTexture("_Emission",myMaps[index]);
+            ChangeDetecter = index;
+        }
+        
+    }
+}
